55a745989def1473451ff7363ea3f9d1e169b3c1
harshghandwani <75669302+harshghandwani@users.noreply.github.com>
2024-07-27 17:55:10 +0530

    Add files via upload

---
+++ precache-manifest.add6d402f712b10f7ff152651b3b24dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363a6b4c2b6f71852db601284bedd909",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a27ab870de961c2bca",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "f8a2c1d110783028bb79",
+    "url": "/static/css/main.f11f4c61.chunk.css"
+  },
+  {
+    "revision": "d4a27ab870de961c2bca",
+    "url": "/static/js/2.4f975c14.chunk.js"
+  },
+  {
+    "revision": "de6bb9b182cb4883b744809bb1ef7215",
+    "url": "/static/js/2.4f975c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319bfefe5a0e23db7d0a",
+    "url": "/static/js/3.ed8653ad.chunk.js"
+  },
+  {
+    "revision": "f8a2c1d110783028bb79",
+    "url": "/static/js/main.b5828ad2.chunk.js"
+  },
+  {
+    "revision": "2b8a2a93d19fb65af568",
+    "url": "/static/js/runtime-main.93ec6c70.js"
+  }
+]);
